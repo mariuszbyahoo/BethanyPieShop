b3e672e43143fad7fc3c9b1f95e410d9c7b80593
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-20 14:59:36 +0200

    Module 4 Creating the Controller

---
+++ BethanyPieShop/Controllers/PieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanyPieShop.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanyPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(
+            IPieRepository pieRepository, 
+            ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
