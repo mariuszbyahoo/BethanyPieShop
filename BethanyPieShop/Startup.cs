5fef56cb92d220f959358c705c37162ef6588be2
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-10-23 09:24:48 +0200

    Module 5 Creating the Real Repository

--- BethanyPieShop/Startup.cs
+++ BethanyPieShop/Startup.cs
@@ -26,8 +26,8 @@ namespace BethanyPieShop
         {
             services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddScoped<IPieRepository, MockPieRepository>();
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddScoped<IPieRepository, PieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddControllersWithViews();
         }
 
